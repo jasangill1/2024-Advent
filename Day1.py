1ad9cb3953a507f7d0041984cf5a6918f0cdc17d
jasangill1 <56135840+jasangill1@users.noreply.github.com>
2024-12-01 15:21:42 -0800

    Day1

--- Day1.py
+++ Day1.py
@@ -7,14 +7,13 @@ Right_number.sort()
 print('the left numbers are:', Left_number)
 print('the right numbers are:', Right_number)
 
-D1 = abs(Left_number[0] - Right_number[0])
-D2 = abs(Left_number[1] - Right_number[1])
-D3 = abs(Left_number[2] - Right_number[2])
-D4 = abs(Left_number[3] - Right_number[3])
-D5 = abs(Left_number[4] - Right_number[4])
-D6 = abs(Left_number[5] - Right_number[5])
+new_list = ([x - y for x, y in zip(Left_number,Right_number)])
 
-total_D = D1 + D2 + D3 + D4 + D5 + D6
+D = 0
 
-print("Total D:", total_D)
+for i in new_list:
+    i += D
+
+print(i)
+
     
