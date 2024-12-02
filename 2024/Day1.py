5443164a5d51ea0aee589a6d186f76ae51c14f23
jasangill1 <56135840+jasangill1@users.noreply.github.com>
2024-12-02 13:15:46 -0800

    part 2 completed

--- 2024/Day1.py
+++ 2024/Day1.py
@@ -1,5 +1,9 @@
-Data = """
-90335   63839
+Data = """58789   28882
+27059   23721
+86784   91527
+72958   13217
+95228   20832
+77437   82573
 39658   98833
 58540   26663
 34246   52806
@@ -9,7 +13,14 @@ Data = """
 rows = Data.strip().split('\n')
 
 x2, y2 = zip(*(map(int, row.split())for row in rows))
-print('Total Distance:',sum([abs(y - x) for x, y in zip(sorted(x2),sorted(y2))]))
 
+similarity = 0
 
+for x in x2:
+    sameChecker = sum(1 for y in y2 if y == x)
+    similarity += x * sameChecker
 
+print(similarity)
+
+print('Total Distance:', sum([abs(y - x)
+      for x, y in zip(sorted(x2), sorted(y2))]))
