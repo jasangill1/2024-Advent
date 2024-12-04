63c1716cba61e4a33afc697204d3bb40889a6a25
jasangill1 <56135840+jasangill1@users.noreply.github.com>
2024-12-03 20:33:58 -0800

    check

--- 2024/Day2.py
+++ 2024/Day2.py
@@ -2,6 +2,8 @@ data ="""1 3 6 7 9
 1 3 6 7 9
 1 3 6 7 9
  1 2 3 4 5 6
+901241059
+ 12345
 """
 
 
@@ -14,12 +16,14 @@ cleaned = [list(map(int, group.split()))for group in grouped]
 count = 0 
 
 for group in cleaned:
-    check = False    
+    check = True    
     firstNum = group[0]
+    increase = all(group[i] > group[i - 1] for i in range(1, len(group)))
     for num in group:
-        if abs(firstNum - num) <= 3:
-            firstNum = num 
-            check = True
+        if abs(firstNum - num) > 3:
+            check = False
+            break
+        firstNum = num 
     if check: 
         count += 1
     
