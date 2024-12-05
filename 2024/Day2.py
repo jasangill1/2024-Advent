bf2db50224e99f3af8de9a97295ffe7174ceeea6
jasangill1 <56135840+jasangill1@users.noreply.github.com>
2024-12-05 09:49:42 -0800

    part 2

--- 2024/Day2.py
+++ 2024/Day2.py
@@ -1,4 +1,4 @@
-data ="""1 3 6 7 9
+data = """1 3 6 7 9
 1 3 6 7 9
 1 5 8 9 2
 """
@@ -8,25 +8,27 @@ grouped = data.strip().split('\n')
 
 cleaned = [list(map(int, group.split()))for group in grouped]
 
-count = 0 
+count = 0
 
 for group in cleaned:
-    check = True    
+    check = True
     firstNum = group[0]
     increase = all(group[i] > group[i - 1] for i in range(1, len(group)))
     decrease = all(group[i] < group[i - 1] for i in range(1, len(group)))
-    
+
     inLimit = all(group[i] < group[i - 1] <= 3 for i in range(1, len(group)))
-    
-    if not(increase or decrease):
-        check = False
-    for num in range(1,len(group)):
-        if abs(group[num] - group[num - 1]) > 3:
-            check = False
-            break
-        firstNum = num 
-    if check: 
+
+    if (increase or decrease) and inLimit:
         count += 1
-    
-    
+    else:
+
+        for num in range(1, len(group)):
+            if abs(group[num] - group[num - 1]) > 3:
+                check = False
+                break
+            firstNum = num
+        if check:
+            count += 1
+
+
 print(count)
